bf5e6e8e1fef3a1ff3cbe185cb3fa218aafd605c
VOFSAI <prodoprovofs@gmail.com>
2026-01-28 07:11:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template
 
-# Linha crucial: cria a instância do Flask
 app = Flask(__name__)
 
-# Rota principal
 @app.route("/")
 def home():
-    return "<h1>TESTE OK — Flask está rodando!</h1>"
+    return render_template("index.html")  # agora Flask vai buscar o arquivo HTML
 
-# Só necessário se rodar localmente (não afeta Render)
 if __name__ == "__main__":
     app.run()
